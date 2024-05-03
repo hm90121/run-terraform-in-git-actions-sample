954a00e9d4c7686b5bf27ada420bef4d00db0b3f
Harshit Mehndiratta <65766301+hm90121@users.noreply.github.com>
2024-05-03 13:23:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 provider "aws" {                    
   region = "ap-northeast-1"
-  access_key = "secrets.AWS_ACCESS_KEY_ID"
-  secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
+  access_key = "AKIAXSPZ33SEY3IRIEW7"
+  secret_key = "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"
 }
 resource "aws_instance" "web" {       
-    ami = "ami-06e46074ae430fba6"
+    ami = "ami-0ed13fc891b36929f"
     instance_type = "t2.micro"
 
     tags = {
